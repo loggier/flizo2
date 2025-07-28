e305146f35112c56ae86f7a36b7f3c4adb34d623
Tracker Parser <info@trackerparser.com>
2025-07-28 03:25:10 +0000

    es posible agregar dos mapas adicional, de la misma forma que el anterio

--- src/components/maps/map-placeholder.tsx
+++ src/components/maps/map-placeholder.tsx
@@ -18,6 +18,7 @@ function MapComponent() {
   const [map, setMap] = React.useState<google.maps.Map | null>(null);
 
   const onLoad = React.useCallback(function callback(mapInstance: google.maps.Map) {
+    // Normal Map
     const osmMapType = new google.maps.ImageMapType({
       getTileUrl: function(coord, zoom) {
         if (!coord || zoom === undefined) {
@@ -28,16 +29,56 @@ function MapComponent() {
         if (x < 0) {
             x = tilesPerGlobe + x;
         }
-        return 'https://mt0.google.com/vt/lyrs=m&x=' + x + '&y=' + coord.y + '&z=' + zoom +
-            '&s=Ga';
+        return `https://mt0.google.com/vt/lyrs=m&x=${x}&y=${coord.y}&z=${zoom}&s=Ga`;
       },
       tileSize: new google.maps.Size(256, 256),
-      name: "OpenStreetMap",
+      name: "Normal",
       maxZoom: 18
+    });
+
+    // Satellite Map
+    const satelliteMapType = new google.maps.ImageMapType({
+        getTileUrl: function(coord, zoom) {
+            if (!coord || zoom === undefined) {
+                return null;
+            }
+            const tilesPerGlobe = 1 << zoom;
+            let x = coord.x % tilesPerGlobe;
+            if (x < 0) {
+                x = tilesPerGlobe + x;
+            }
+            const subdomain = ['mt0', 'mt1', 'mt2', 'mt3'][coord.x % 4];
+            return `https://${subdomain}.google.com/vt/lyrs=y&x=${x}&y=${coord.y}&z=${zoom}&s=Ga`;
+        },
+        tileSize: new google.maps.Size(256, 256),
+        name: 'Satellite',
+        maxZoom: 20
+    });
+
+    // Traffic Map
+    const trafficMapType = new google.maps.ImageMapType({
+        getTileUrl: function(coord, zoom) {
+            if (!coord || zoom === undefined) {
+                return null;
+            }
+            const tilesPerGlobe = 1 << zoom;
+            let x = coord.x % tilesPerGlobe;
+            if (x < 0) {
+                x = tilesPerGlobe + x;
+            }
+            const subdomain = ['mt0', 'mt1', 'mt2', 'mt3'][coord.x % 4];
+            return `https://${subdomain}.google.com/vt/lyrs=m@221097413,traffic&x=${x}&y=${coord.y}&z=${zoom}&s=Ga`;
+        },
+        tileSize: new google.maps.Size(256, 256),
+        name: 'Traffic',
+        maxZoom: 20
     });
     
     mapInstance.mapTypes.set("OSM", osmMapType);
+    mapInstance.mapTypes.set("SATELLITE", satelliteMapType);
+    mapInstance.mapTypes.set("TRAFFIC", trafficMapType);
     mapInstance.setMapTypeId("OSM");
+
     setMap(mapInstance);
   }, []);
 
