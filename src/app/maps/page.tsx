532ab7e56e4b732f5750ac7dd8c56a74b7cd0431
Tracker Parser <info@trackerparser.com>
2025-07-28 03:13:35 +0000

    agreguemos el mapa de google maps pero de esta forma
    si te das cuenta l

--- src/app/maps/page.tsx
+++ src/app/maps/page.tsx
@@ -1,12 +1,12 @@
 
-import MapPlaceholder from "@/components/maps/map-placeholder";
+import MapComponent from "@/components/maps/map-placeholder";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 
 export default function MapsPage() {
   return (
     <div className="relative h-full w-full">
-      <MapPlaceholder />
+      <MapComponent />
       <div className="absolute top-4 left-4">
         <Button variant="outline" size="icon" className="bg-background rounded-full shadow-md hover:bg-primary/90 hover:text-primary-foreground">
           <Menu className="h-6 w-6" />
--- src/components/maps/map-placeholder.tsx
+++ src/components/maps/map-placeholder.tsx
@@ -1,16 +1,67 @@
 
-import Image from "next/image";
+"use client";
 
-export default function MapPlaceholder() {
-  return (
-    <div className="h-full w-full">
-      <Image
-        src="https://placehold.co/1080x1920.png"
-        alt="World Map"
-        layout="fill"
-        objectFit="cover"
-        data-ai-hint="world map"
-      />
-    </div>
-  );
+import React from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '100%'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+const mapOptions = {
+  zoomControl: false,
+  streetViewControl: false,
+  mapTypeControl: false,
+  fullscreenControl: false,
+};
+
+function MapComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "" // We don't need an API key for this method
+  });
+
+  const onLoad = React.useCallback(function callback(map: google.maps.Map) {
+    const osmMapType = new google.maps.ImageMapType({
+      getTileUrl: function(coord, zoom) {
+        if (!coord || zoom === undefined) {
+            return null;
+        }
+        const tilesPerGlobe = 1 << zoom;
+        let x = coord.x % tilesPerGlobe;
+        if (x < 0) {
+            x = tilesPerGlobe + x;
+        }
+        return 'https://mt0.google.com/vt/lyrs=m&x=' + x + '&y=' + coord.y + '&z=' + zoom +
+            '&s=Ga';
+      },
+      tileSize: new google.maps.Size(256, 256),
+      name: "OpenStreetMap",
+      maxZoom: 18
+    });
+    
+    map.mapTypes.set("OSM", osmMapType);
+    map.setMapTypeId("OSM");
+
+  }, []);
+
+  return isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        options={mapOptions}
+      >
+        { /* Child components, like markers, info windows, etc. */ }
+      </GoogleMap>
+  ) : <></>
 }
+
+export default React.memo(MapComponent);
