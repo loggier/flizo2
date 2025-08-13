00b55a47f3a8afb8e0052399f389e42722ff7b25
Tracker Parser <info@trackerparser.com>
2025-08-13 07:38:28 +0000

    asi lo uso en ionic en el front <ion-row *ngFor="let i of getEvents(hist

--- src/components/history/history-detail-row.tsx
+++ src/components/history/history-detail-row.tsx
@@ -43,24 +43,27 @@ export default function HistoryDetailRow({ group, getStatusText }: HistoryDetail
         };
     }, [group]);
     
-    const timeToDisplay = (group.status === 2 || group.status === 3 || group.status === 4 || group.status === 5) ? (group.items[0]?.time || group.raw_time) : (group.raw_time);
-    const [date, time] = (timeToDisplay || " ").split(" ");
+    const timeToDisplay = group.raw_time || " ";
+    const [date, time] = (timeToDisplay).split(" ");
 
     return (
-        <div className="grid grid-cols-[auto_minmax(0,_1fr)_minmax(0,_2fr)] items-start gap-x-3 text-sm py-2 border-b last:border-b-0">
-             <div className="flex items-center justify-center w-6 h-6 bg-gray-200 text-gray-700 rounded-sm font-mono text-xs font-bold mt-1">
+        <div className="grid grid-cols-[auto_minmax(0,_1fr)_minmax(0,_1fr)_minmax(0,_2fr)] items-start gap-x-2 text-xs py-2 border-b last:border-b-0">
+             <div className="flex items-center justify-center w-6 h-6 bg-gray-200 text-gray-700 rounded-sm font-mono font-bold mt-1">
                 {getStatusText(group.status)}
             </div>
             
-            <div className="text-gray-800">
-              <p className="font-semibold">{date}</p>
-              <p className="font-semibold">{time}</p>
-              {group.status === 2 && (
-                <p className="font-normal text-primary text-xs pt-1">{group.time}</p>
-              )}
-               {group.status === 5 && group.items[0]?.message && (
-                <p className="font-normal text-gray-600 text-xs pt-1">{group.items[0].message}</p>
-              )}
+            <div className="text-gray-800 font-semibold">
+              <p>{date}</p>
+              <p>{time}</p>
+            </div>
+
+            <div className="text-gray-600 font-medium">
+                {(group.status === 1 || group.status === 2 || group.status === 3 || group.status === 4) && (
+                    <p className="text-primary">{group.time}</p>
+                )}
+                {group.status === 5 && (
+                    <p>{group.items[0]?.message}</p>
+                )}
             </div>
 
             <div className="text-gray-600 break-words">
@@ -69,3 +72,4 @@ export default function HistoryDetailRow({ group, getStatusText }: HistoryDetail
         </div>
     );
 }
+
