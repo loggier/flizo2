df73fd6028c3783ca14da237ca4373438230d81d
Tracker Parser <info@trackerparser.com>
2025-08-23 06:57:55 +0000

    hay un detalles estas haciendo un autozoom donde va el punto, no debe ha

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -89,9 +89,6 @@ function HistoryPageContent() {
                     const nextPosition = routePath[nextIndex];
                     if (nextPosition) {
                         map.panTo(nextPosition);
-                        if (map.getZoom()! < 15) {
-                            map.setZoom(18);
-                        }
                     }
 
                     return nextIndex;
